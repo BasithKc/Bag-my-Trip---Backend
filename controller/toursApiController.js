7d564ae4c4fdf5b46e0d6a1b2cc70dd5faee4704
basith <abdulbasithkc313@gmail.com>
2024-11-06 11:32:23 +0530

    Updated update tour function for admin

--- controller/toursApiController.js
+++ controller/toursApiController.js
@@ -237,6 +237,9 @@ module.exports = {
 
       // handle gallery images uplaod
       if (req.files.gallery) {
+        //First delete from s3
+        await deleteImages(transformedTourData.gallery)
+
         const galleryUrls = await Promise.all(req.files.gallery.map(async (file) => {
           return await uploadFile(file, 'gallery-images')
         }))

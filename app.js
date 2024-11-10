22796380057d48362f1867a8eea86e36a64764a3
basith <abdulbasithkc313@gmail.com>
2024-11-10 17:20:46 +0530

    updated app.js

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // CORS configuration
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.bagmytrip.in', 'https://bagmytrip.in'],
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/admin/auth', adminAuthRoute)

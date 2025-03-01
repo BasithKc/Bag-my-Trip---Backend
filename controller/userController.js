f41f9531d51cad9349735c3ccfa52079e137fc35
basith <abdulbasithkc313@gmail.com>
2025-03-01 17:39:52 +0530

    cleaning

--- controller/userController.js
+++ controller/userController.js
@@ -6,6 +6,7 @@ const Book = require('../models/book')
 const transporter = require('../config/nodemailer')
 
 module.exports = {
+  
   //Function for fetch all tours
   getAllTours: async (req, res) => {
     try {

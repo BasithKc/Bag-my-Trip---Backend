8ecf5d2f31f837a2dcacfaa7664e47adc1a3d8d9
Abdul Basith <143891409+BasithKc@users.noreply.github.com>
2026-01-01 12:29:48 +0530

    Add export for comparePassword function

--- utils/validation.js
+++ utils/validation.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 async function comparePassword(hashedPassword, password) {
   return await bcrypt.compare(password, hashedPassword)
 }
+
 module.exports = {
   comparePassword
 }
